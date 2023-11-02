423c75e44a913f6a8aa7a2fdec2a1a6c76ed546c
Arun Thirunav <arunthirunavukkarasu96@gmail.com>
2023-11-02 18:12:49 +0530

    Added Logging Module

--- appv2.py
+++ appv2.py
@@ -1,10 +1,8 @@
-import PySimpleGUI as sg
-import sys
 import os
 import hashlib
+import logging
+import PySimpleGUI as sg
 from cryptography.fernet import Fernet
-from zipfile import ZipFile
-import time
 
 sg.theme('Reddit')
 t1=sg.Input("", key='sign_lineEdit', readonly='True')
@@ -24,48 +22,81 @@ layout = [[sg.TabGroup([
 
 
 window = sg.Window('Danfoss Secure Manufacturing Tool', layout)
-previous_filename = ""
 key = "R6Tv4of5jSC9jHQDPqzrZCwDElE4xklHRb9knNs5DT4="
-fernet = Fernet(key)
+logging.basicConfig(level=logging.DEBUG)
 CIPHER_KEY_LEN = 64
 
-def signButtonHandler(filename)->None:
-    global previous_filename
-    print("Enc_File_Location: ", filename)
+class Cryptography:
+    def __init__(self, key):
+        self.fernet = Fernet(key)
+
+    def encrypt(self, data):
+        return self.fernet.encrypt(data)
+    
+    def decrypt(self, data):
+        return self.fernet.decrypt(data)
+
+def sign_button_handler(filename)->None:
+    """
+    Encrypts the contents of a file using a Fernet key and creates a new encrypted file with the extension ".cro".
+    Calculates the SHA256 hash of the input file and writes it to the encrypted file.
+    Displays a popup message indicating whether the encrypted file was successfully created or not.
+
+    Args:
+        filename (str): The path of the file to be encrypted.
+
+    Returns:
+        None
+    """
+    logging.debug("Enc_File_Location: %s", filename)
     enc_path = os.path.dirname(filename)
-    enc_filename = (os.path.basename(filename)).split(".")[0] + ".cro"
-    enc_filename = enc_path+"/"+enc_filename
+    enc_filename = os.path.join(enc_path, (os.path.basename(filename)).split(".")[0] + ".cro")
     
     try:
-        with open(filename,"rb") as f:
-            bytes = f.read()
+        with open(filename,"rb") as raw_file:
+            bytes = raw_file.read()
         hash_bytes = hashlib.sha256(bytes).hexdigest()
-        encrypted = fernet.encrypt(bytes)
+
+        encryptor = Cryptography(key)
+        encrypted = encryptor.encrypt(bytes)
+
         with open(enc_filename, 'wb') as encrypted_file:
             try:
                 encrypted_file.write(bytearray(hash_bytes, 'utf-8'))
                 encrypted_file.write(encrypted)
                 sg.popup_ok("Encrypted File Created")
-            except:
+            except :
                 sg.popup_error("Failed to Create Encrypted File")
-    except FileNotFoundError or FileExistsError:
+    except FileNotFoundError:
         sg.popup_error("File Not Found. ")
+    except FileExistsError:
+        sg.popup_error("File Already Exists. ")
+def verify_button_handler(file_name: str) -> None:
+    """
+    Verify the integrity of a file.
 
-def verifyButtonHandler(file_name)->None:
+    Args:
+        file_name (str): The name of the file to be verified.
+
+    Returns:
+        None: The function does not return any value. It only displays messages indicating the result of the file verification process.
+    """
+
     ext = (os.path.basename(file_name)).split(".")[1]
     if ext == "cro": 
         try:
-            with open(file_name, 'rb') as f:
-                cipher_text = f.read()
+            with open(file_name, 'rb') as enc_file:
+                cipher_text = enc_file.read()
             hash_bytes = cipher_text[0:CIPHER_KEY_LEN]
-            decrypted = fernet.decrypt(cipher_text[CIPHER_KEY_LEN:])
+            decryptor = Cryptography(key)
+            decrypted = decryptor.decrypt(cipher_text[CIPHER_KEY_LEN:])
             calculated_hashbytes =  hashlib.sha256(decrypted).hexdigest()
-            
+
             if bytes(calculated_hashbytes, 'utf-8') == hash_bytes:
                 sg.popup_ok("File Verification Success")
             else:
                 sg.popup_error("File Corrupted")
-        except:
+        except (FileExistsError, FileNotFoundError):
             sg.popup_error("Cannot Verify the File")
     else:
         sg.popup_error("Invalid File Format")
@@ -76,9 +107,9 @@ if __name__ == "__main__":
         if event == sg.WIN_CLOSED:
             break
         if event == "Sign":
-            signButtonHandler(values['sign_lineEdit'])
+            sign_button_handler(values['sign_lineEdit'])
         if event == "Verify":
-            verifyButtonHandler(values['verify_lineEdit'])
+            verify_button_handler(values['verify_lineEdit'])
 
 window.close()
 
--- rough.py
+++ rough.py
@@ -1,20 +1,4 @@
-import os
-from zipfile import ZipFile
+import logging
 
-
-filepath = "/home/arun/Programming/Secure_Manufacturing/080N0403.bin"
-signature_filepath = "/home/arun/Programming/Secure_Manufacturing/080N0403_signature"
-cur_path = os.path.dirname(filepath)
-filename = os.path.basename(filepath)
-signature_filename = os.path.basename(signature_filepath)
-
-with ZipFile(cur_path+"/"+filename.split(".")[0]+".akds", 'w') as zip_object:
-   # Adding files that need to be zipped
-      zip_object.write(filename)
-      zip_object.write(signature_filename)
-
-# Check to see if the zip file is created
-if os.path.exists('cur_path+"/"+filename.split(".")[0]+".zip"'):
-   print("ZIP file created")
-else:
-   print("ZIP file not created")
+logging.basicConfig(level=logging.DEBUG)
+logging.debug("Hello ")
